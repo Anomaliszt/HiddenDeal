55844084be12c9497cc0eca8064b40de1f2339a6
Anomaliszt <xavier.sanchez44@orange.fr>
2025-03-06 07:53:42 +0100

    Add script for creating auctions via API

---
+++ backend/tests/create_auction.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+TITLE=${1:-"test"}
+DESCRIPTION=${2:-"This is a test auction created via the API"}
+ITEM_VALUE=${4:-500}
+EXPIRY_DATE="2025-02-28T17:49:30"
+
+curl -X POST http://127.0.0.1:5000/api/auctions/ \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $TOKEN" \
+-d '{
+  "title": "'"$TITLE"'",
+  "description": "'"$DESCRIPTION"'",
+  "starting_price": '"$ITEM_VALUE"',
+  "item_value": '"$ITEM_VALUE"',
+  "expires_at": "'"$EXPIRY_DATE"'"
+}'
