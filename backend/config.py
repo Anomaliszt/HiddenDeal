57c24ca335e3b34afb41eef62f8883580d4414aa
Anomaliszt <xavier.sanchez44@orange.fr>
2025-03-17 09:44:44 +0100

    removed unecessary files for landing page

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///reverse_auction.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your-jwt-secret-key'
